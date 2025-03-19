d74a03ba55190ec7cb087ab9fbefc9fe5746f76b
Chris Tutorials <christutorialsyt@gmail.com>
2025-03-19 09:07:40 -0400

    shifting towards issues push in validate

--- ui/popups/max_value_button.gd
+++ ui/popups/max_value_button.gd
@@ -2,8 +2,8 @@ extends Button
 
 @export var value_control : Control
 
-func _ready():
+func _ready() -> void:
 	pressed.connect(_on_pressed)
 
-func _on_pressed():
+func _on_pressed() -> void:
 	value_control.value = value_control.max_value
